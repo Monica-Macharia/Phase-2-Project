2e86b9163842030c25347cbbbc75f6be72c3df86
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-28 22:45:50 +0300

    changed image

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,9 @@ function Home(){
       backgroundImage: `url("https://images.unsplash.com/photo-1556379069-7c1b1b8990b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YWZyaWNhbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60")` 
     }}></div> */}
     <div className= "container">
-       <h2>
+       <h1>
            Offering the best African Dishes
-        </h2> 
+        </h1> 
         <p>
         <span> Dishes prepared in Africana are carefully selected </span><br/>from hundreds of tribes accross the continent.
            <span><br/> Each made with love, for the Mama Africa.</span>
@@ -19,7 +19,7 @@ function Home(){
             Menu
         </button>
         <div>
-         <img id = "main" src="https://images.unsplash.com/photo-1598452498516-76a50b446f23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjg0fHxyZXN0YXVyYW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt = "hotel"/> 
+         <img id = "main" src="https://images.unsplash.com/photo-1565695040813-8812dba10aca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTc1fHxyZXN0YXVyYW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt = "hotel"/> 
         </div>
     </div>
 
