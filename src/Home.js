40cf74040ab12e6a301c3274168dde3598593236
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-28 22:14:09 +0300

    styling main pae

--- src/Home.js
+++ src/Home.js
@@ -15,11 +15,12 @@ function Home(){
         <span> Dishes prepared in Africana are carefully selected </span><br/>from hundreds of tribes accross the continent.
            <span><br/> Each made with love, for the Mama Africa.</span>
         </p>
-        
         <button className= "action">
             Menu
         </button>
-        {/* <img src=""/> */}
+        <div>
+         <img id = "main" src="https://images.unsplash.com/photo-1598452498516-76a50b446f23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjg0fHxyZXN0YXVyYW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt = "hotel"/> 
+        </div>
     </div>
 
     </div>
