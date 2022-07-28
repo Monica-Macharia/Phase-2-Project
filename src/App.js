9f84fb59616b8f690f5baafb7850e32fd2c82343
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-28 11:34:38 +0300

    created components' skeletons and imported in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import Home from './Home.js'
+import Home from './Home.js';
+import Contact from './Contact.js';
+import Gallery from './Gallery.js';
+import Menu from '.Menu.js';
+import Testimonials from './Testimonials.js';
 
 function App() {
   return (
