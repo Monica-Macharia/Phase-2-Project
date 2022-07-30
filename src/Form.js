86968f16828bcef9192126aad04216f140046a8f
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-30 22:25:47 +0300

    created a table to store  get and post requests from a local db.json

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Form(){
  return(
-     <>
+     <div>
      <div>
      <h1>Dine with us</h1>
      </div>
@@ -20,10 +20,30 @@ function Form(){
              <input type="text" name= "food"/>
          </label>
          <label>
+             Book:
+             <input type="text" name= "text"/>
+         </label>
+         <label>
              <input type="submit" name="Submit" />
          </label>
          </form>
-     </>
+     <div className = "entry">
+         <table>
+             <tr>
+                 <th>Date</th>
+                 <th>Seat Number</th>
+                 <th>Food Category</th>
+                 <th>Book</th>
+             </tr>
+             <tr>
+                 <td>ggyyg</td>
+                 <td>vvhbj</td>
+                 <td>vvhh</td>
+                 <td>gvvvh</td>
+             </tr>
+         </table>
+     </div>
+     </div>
  )
 }
 
