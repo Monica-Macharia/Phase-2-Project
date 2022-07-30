22254bd59238a25932b86e6c8fcba016b270f8d2
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-30 07:44:28 +0300

    fetch function

--- src/Dining.js
+++ src/Dining.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 
 function Dining(){
+    const[data, setData] = useState([]);
+    const [onLoad, setOnLoad]= useState(false);
+
+   useEffect(()=>{
+    
+     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+    .then((res) => res.json())
+    .then((json)=> { 
+        
+        setData(json.categories);
+        setOnLoad(true);
+    });
+    }, []);
+    if (!onLoad) return <h3>Loading...</h3>;
     return (
     <div>
- 
+        //map the data please
     </div>
 )
 }
